aa172ed9222e9a66122c2f936944cf9bba0e2ac8
engida32 <engida32lishan@gmail.com>
2021-12-24 21:45:35 +0300

    💄   added pet componnent

---
+++ src/Pet.js
@@ -0,0 +1,17 @@
+//import React from "react";
+
+// const Pet = (props) => {
+//     return React.createElement("div", {}, [
+//         React.createElement("h2", {}, props.name),
+//         React.createElement("h3", {}, props.animal),
+//         React.createElement("h3", {}, props.bred),
+//     ]);
+// };
+const Pet = (props) => {
+    return <div>
+        <h2>{props.name}</h2>
+        <h3>{props.animal}</h3>
+        <h3>{props.bred}</h3>
+    </div>
+};
+export default Pet;
