4635d621e54452040a6c721dae5e1edcb475cd3a
engida32 <engida32lishan@gmail.com>
2021-12-29 19:42:44 +0300

    class component withRouter

---
+++ src/Details.js
@@ -0,0 +1,55 @@
+// import { useParams } from "react-router-dom";
+import { Component } from "react";
+import { withRouter } from "react-router-dom";
+
+class Details extends Component {
+    constructor() {
+        super();
+        this.state = { loading: true };
+
+    }
+    async componentDidMount() {
+        const res = await fetch(
+             // eslint-disable-next-line react/prop-types
+             `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
+        );
+        const json = await res.json();
+        this.setState(Object.assign({ loading: false }, json.pets[0]));
+
+        // this.setState({
+        //     loading: false,
+        //     name: json.pets[0].name,
+        //     breed: json.pets[0].breed,
+        //     animal: json.pets[0].animal,
+        //     city: json.pets[0].city,
+        //     state: json.pets[0].state,
+        //     description: json.pets[0].description,
+
+        // })
+
+    }
+    render() {
+        console.log(this.state);
+        if (this.state.loading) {
+            return <h2> loading</h2>;
+        }
+        const { animal, breed, city, state, description, name } = this.state;
+        return (
+            <div className="details">
+                <div>
+                    <h1>{animal}</h1>
+                    {/* using template string of js*/}
+                    <h2>{`${animal}-${breed}-${city},${state}`}</h2>
+                    {/* using  jsc*/}
+                    {/* <h2>{animal} - {breed} - {city},{state}</h2> */}
+                    <button>Adopt {name}</button>
+                    <p>{description}</p>
+                </div>
+
+            </div>
+        );
+
+
+    }
+}
+export default withRouter(Details);
