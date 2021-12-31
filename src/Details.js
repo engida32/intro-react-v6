c051326afb84a44463a2a5ff0d9d1476afcaffad
engida32 <engida32lishan@gmail.com>
2021-12-31 10:03:31 +0300

    error boundary

--- src/Details.js
+++ src/Details.js
@@ -1,17 +1,21 @@
 // import { useParams } from "react-router-dom";
 import { Component } from "react";
 import { withRouter } from "react-router-dom";
+import Carousel from './Carousel';
+import ErrorBoundary from "./ErrorBoundary";
+
 
 class Details extends Component {
-    constructor() {
-        super();
-        this.state = { loading: true };
+    // constructor() {
+    //     super();
+    //     this.state = { loading: true };
 
-    }
+    // }
+    state = { loading: true };
     async componentDidMount() {
         const res = await fetch(
-             // eslint-disable-next-line react/prop-types
-             `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
+            // eslint-disable-next-line react/prop-types
+            `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
         );
         const json = await res.json();
         this.setState(Object.assign({ loading: false }, json.pets[0]));
@@ -33,9 +37,11 @@ class Details extends Component {
         if (this.state.loading) {
             return <h2> loading</h2>;
         }
-        const { animal, breed, city, state, description, name } = this.state;
+        const { animal, breed, city, state, description, name, images } = this.state;
+        // throw new Error("its broken");
         return (
             <div className="details">
+                <Carousel images={images} />
                 <div>
                     <h1>{animal}</h1>
                     {/* using template string of js*/}
@@ -52,4 +58,12 @@ class Details extends Component {
 
     }
 }
-export default withRouter(Details);
+const DetailsWithRouter = withRouter(Details);
+
+export default function DetailsErrorBoundary(props) {
+    return (
+        <ErrorBoundary>
+            <DetailsWithRouter {...props} />
+        </ErrorBoundary>
+    );
+}
